14b8640d5d033bf6be5b71d05253f573678de2ca
isnirp <pramankwah@gmail.com>
2020-12-02 15:05:30 +0100

    banner partials

---
+++ src/component/partials/Banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <React.Fragment>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">COVID-19 Discovery</h1>
+          <p className="lead">Short notes on the scope of the covid</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
